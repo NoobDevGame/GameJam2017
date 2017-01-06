7571f092e198ad0e8cc0f6d909fa8b509412d9f7
Lassi <csharplassi@posteo.de>
2017-01-06 21:34:40 +0100

    World und Simulation im Core anlgelegt

---
+++ GameJam2017/NoobFight.Core/Simulation/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NoobFight.Contract.Entities;
+using NoobFight.Contract.Map;
+using NoobFight.Contract.Simulation;
+using NoobFight.Core.Entities;
+
+namespace NoobFight.Core.Simulation
+{
+    public class World : IWorld
+    {
+        public IMap Map { get; }
+
+        private List<Entity> _entities = new List<Entity>();
+        public IEnumerable<IEntity> Entities => _entities;
+
+
+    }
+}
