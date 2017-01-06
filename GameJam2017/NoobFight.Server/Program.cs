8bc8dea27bd302ca216f4ac72dbb1f2afa6af145
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-01-06 22:38:48 +0100

    NoobFight.Server: Add Server and Communication Handling with very Importing and powerfull Message Handling. Is a wonderfull sourcecode from the greatest developer in the world.

--- GameJam2017/NoobFight.Server/Program.cs
+++ GameJam2017/NoobFight.Server/Program.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NoobFight.Server
 {
     class Program
     {
+        static Server server;
+        static EventWaitHandle eventWait;
+
         static void Main(string[] args)
         {
+            eventWait = new EventWaitHandle(false, EventResetMode.ManualReset);
+            server = new Server();
+            server.Start();
+            eventWait.WaitOne();
         }
     }
 }
---
+++ GameJam2017/NoobFight.Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using NoobFight.Core.Network;
+
+namespace NoobFight.Server
+{
+    internal class Server
+    {
+        TcpListener listener;
+        ConcurrentDictionary<int, Client> clients;
+
+        public Server()
+        {
+            listener = new TcpListener(IPAddress.Any, 667);
+            clients = new ConcurrentDictionary<int, Client>();
+        }
+
+        public void Start()
+        {
+            listener.BeginAcceptTcpClient(HandShake, null);
+        }
+
+        private void HandShake(IAsyncResult ar)
+        {
+            var tcpClient = listener.EndAcceptTcpClient(ar);
+            listener.BeginAcceptTcpClient(HandShake, null);
+
+            Task.Run(() =>
+            {
+                bool added = false;
+                var client = new Client(tcpClient, clients.Count + 1);
+                while (!added)
+                    added = clients.TryAdd(client.ID, client);
+
+                client.BeginReceived();
+                client.OnMessageReceived += client_OnMessageReceived;
+                
+            });
+        }
+
+        private void client_OnMessageReceived(object sender, NetworkMessage message)
+        {
+            throw new NotImplementedException("Uuups I did it again >:" + message.ToString());
+        }
+        
+    }
+}
