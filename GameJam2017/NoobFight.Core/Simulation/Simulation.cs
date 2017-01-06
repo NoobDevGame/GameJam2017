809f5371129500fe84bfba86957bc0f5f6361fb1
Lassi <csharplassi@posteo.de>
2017-01-06 21:44:46 +0100

    Simulation erweitert um Worlds

--- GameJam2017/NoobFight.Core/Simulation/Simulation.cs
+++ GameJam2017/NoobFight.Core/Simulation/Simulation.cs
@@ -1,11 +1,19 @@
-﻿using NoobFight.Contract.Simulation;
+﻿using System.Collections.Generic;
+using NoobFight.Contract.Simulation;
 
 namespace NoobFight.Core.Simulation
 {
     public class Simulation : ISimulation
     {
-        public void CreateNewWorld()
+        private List<World> _worlds = new List<World>();
+        public IEnumerable<IWorld> Worlds => _worlds;
+
+        public IWorld CreateNewWorld()
         {
+            World newworld = new World();
+            _worlds.Add(newworld);
+
+            return newworld;
 
         }
     }
