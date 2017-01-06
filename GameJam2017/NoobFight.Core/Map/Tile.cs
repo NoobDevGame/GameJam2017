346a0285753cfece9538fa7974ac35a77ef9814f
Daniel <henk.daniel98@gmail.com>
2017-01-06 22:28:42 +0100

    FIX: Tile

--- GameJam2017/NoobFight.Core/Map/Tile.cs
+++ GameJam2017/NoobFight.Core/Map/Tile.cs
@@ -1,9 +1,18 @@
-﻿using NoobFight.Contract.Map;
+﻿using System;
+using NoobFight.Contract.Map;
 
 namespace NoobFight.Core.Map
 {
     public class Tile : ITile
     {
+        public int Asset { get; private set; }
 
+        public bool Collidable { get; private set; }
+
+        public Tile(int Asset, bool Collidable)
+        {
+            this.Asset = Asset;
+            this.Collidable = Collidable;
+        }
     }
 }
