da79266b41343d7e6f29c0c071556dc887e56f41
Lassi <csharplassi@posteo.de>
2017-01-06 21:21:47 +0100

    Item angelegt

---
+++ GameJam2017/NoobFight.Core/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using NoobFight.Contract.Items;
+
+namespace NoobFight.Core.Items
+{
+    public class Item : IItem
+    {
+        public Item(string name, string textureName)
+        {
+            Name = name;
+            TextureName = textureName;
+        }
+
+        public string Name { get; private set; }
+        public string TextureName { get; private set; }
+    }
+}
