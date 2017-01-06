a72372eb5cb5962a700d3ec28d987db454ae261c
Daniel <henk.daniel98@gmail.com>
2017-01-06 22:57:25 +0100

    EDIT: Loader

--- GameJam2017/NoobFight.Core/Map/Loader.cs
+++ GameJam2017/NoobFight.Core/Map/Loader.cs
@@ -1,9 +1,34 @@
-﻿namespace NoobFight.Core.Map
+﻿using System.IO;
+using Newtonsoft.Json;
+using NoobFight.Contract.Map;
+
+namespace NoobFight.Core.Map
 {
     public class Loader
     {
-        public Loader()
+        private string _Path;
+
+        public Loader(string Path = null)
         {
+            this._Path = GetPath(Path);
+        }
+
+        public void SaveMap(IMap map)
+        {
+            File.WriteAllText(this._Path, JsonConvert.SerializeObject(map));
+        }
+
+        public IMap LoadMap()
+        {
+            return JsonConvert.DeserializeObject<IMap>(File.ReadAllText(this._Path));
+        }
+
+        private string GetPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return path = Path.Combine(Directory.GetCurrentDirectory(), "NoobFightMap");
+            else
+                return path;
         }
     }
 }
